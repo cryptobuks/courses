48fac842624dba09d084e0038e61c3a0cc02e0f0
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-11-18 11:51:35 +0100

    style

--- .history/index_20221118115118.php
+++ .history/index_20221118115118.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <link rel="stylesheet" href="/wp-content/themes/courses-1/style.css"/>
 
     <?php wp_head(); ?>
   </head>
