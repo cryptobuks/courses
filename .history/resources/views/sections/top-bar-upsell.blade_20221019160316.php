2375de92fdacb005ea734bc386707b5b4807507d
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-11-19 12:44:58 +0100

    thank you

--- .history/resources/views/sections/top-bar-upsell.blade_20221019160316.php
+++
@@ -1,9 +0,0 @@
-@if($phoneNumber)
-<header>
-    <div class="w-full p-4  items-center flex justify-between mx-auto container shadow-md">
-        <img class="h-10 no-lazyload shadow-lg	" src="{{$logoColor}}"></img>
-        <p class="text-black"> {{$phoneNumber}} </p>
-    </div>
-    
-</header>
-@endif
