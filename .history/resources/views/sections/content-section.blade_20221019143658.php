1f1d9ceda6fab54902ad49ce39116a0d995178a3
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-11-21 10:04:03 +0100

    first section added

--- .history/resources/views/sections/content-section.blade_20221019143658.php
+++
@@ -1,6 +0,0 @@
-<section class="flex justify-center">
-    <div class="width-780 max-w-contentwidth p-4">
-        {!! $firstContent !!}</div>
-        @php(the_content())
-
-</section>
--- .history/resources/views/sections/content-section.blade_20221019155710.php
+++
@@ -1,4 +0,0 @@
-<section class="flex justify-center">
-    <div class="width-780 max-w-contentwidth p-4">
-        {!! $firstContent !!}</div>
-</section>
