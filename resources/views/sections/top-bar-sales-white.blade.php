baae39145acfa913f53fb59d163d7429579437bd
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-12-18 11:04:42 +0100

    image

--- resources/views/sections/top-bar-sales-white.blade.php
+++ resources/views/sections/top-bar-sales-white.blade.php
@@ -1,7 +1,7 @@
 @if($phoneNumber)
 <header class="bg-white sticky top-0 z-50">
     <div class="w-full p-2 items-center flex justify-between mx-auto max-w-pageWidth">
-        <img class="md:h-10 h-6 no-lazyload flex" src="{{$logoColor}}"></img>
+        <img class="md:h-10 h-6 no-lazyload md:flex hidden" src="{{$logoColor}}"></img>
         @include('partials.salespages-info')
     </div>
 </header>
