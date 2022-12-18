442df75640e7f72ec87c1d27a5664aa1205eb4e8
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-12-18 02:27:53 +0100

    limited time offer

--- resources/views/sections/limited-offer.blade.php
+++ resources/views/sections/limited-offer.blade.php
@@ -1,9 +1,8 @@
 <section class=" justify-center max-w-contentwidth mx-auto mb-10 ">
     <div class="border-dashed border-4 border-borderColorRed rounded-t-full bg-[#FFF6F6] p-4 w-fit mx-auto -mb-1 border-b-0	sticky h-3/6">
-        <img class="h-16" src="/wp-content/themes/courses-1/resources/images/seen-on-tv.png"></img>    
+        <img class="h-16" src="/wp-content/themes/courses-1/resources/images/limited-time-offer.png"></img>    
     </div>
     <div class=" border-dashed border-4 border-borderColorRed rounded-md p-4 sm:p-4 bg-[#FFF6F6] w-full ">
     {!! $limitedOffer !!}</div>
 
-    
 </section>
