669baabde0d73c154794cbc89af5e39d831f31a9
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-12-19 14:48:31 +0100

    s

--- resources/views/sections/first-section.blade.php
+++ resources/views/sections/first-section.blade.php
@@ -1,8 +1,8 @@
 
 <section class="">
     <div>
-        <h1 class="justify-items-center !font-extrabold md:text-center text-left md:py-1 text-4xl md:!text-5xl mb-4 text-black ">{{$mainHeading}}</h1>
-        <h2 class=" justify-items-center md:!font-semibold !font-normal text-black md:text-center text-left md:mb-14 mb-4 !text-xl md:!text-2xl ">{{$subheading}}</h2>
+        <h1 class="justify-items-center !font-extrabold md:text-center text-left md:py-1 text-4xl md:!text-5xl mb-4 text-black ">{!!  $mainHeading !!}</h1>
+        <h2 class=" justify-items-center md:!font-semibold !font-normal text-black md:text-center text-left md:mb-14 mb-4 !text-xl md:!text-2xl ">{!! $subheading !!}</h2>
     </div>
    @include('partials.program-section-new')
    @include('partials.as-seen-on')
