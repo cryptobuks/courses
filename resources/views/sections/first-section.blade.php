169f703bfdc138f93ad5545e85bfad639cdf7d8c
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-12-21 12:49:54 +0100

    changes

--- resources/views/sections/first-section.blade.php
+++ resources/views/sections/first-section.blade.php
@@ -2,7 +2,7 @@
 <section class="">
     <div class=" text-center">
         @include('partials.top-heading')
-        <h1 class="justify-items-center !font-extrabold md:text-center text-left text-4xl md:!text-5xl mb-4 text-black "></h1>
+        <h1 class="justify-items-center !font-extrabold md:text-center !text-left text-4xl md:!text-5xl mb-4 text-black "></h1>
         <div>{!!$mainHeading!!}</div>
         <h2 class=" justify-items-center md:!font-semibold !font-normal text-black md:text-center text-left md:mb-14 mb-4 !text-xl md:!text-2xl ">{!!$subheading!!}</h2>
     </div>
