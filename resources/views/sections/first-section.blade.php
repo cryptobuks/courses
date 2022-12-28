eccc0d7672fbe3312b96a6706f54ddd3c7e03c18
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-12-28 21:04:16 +0100

    fix

--- resources/views/sections/first-section.blade.php
+++ resources/views/sections/first-section.blade.php
@@ -1,10 +1,11 @@
 
-<section>
+<section class="">
     <div class=" text-center">
         @include('partials.top-heading')
         <div>{!!$mainHeading!!}</div>
         <div>{!!$subheading!!}</div>
     </div>
+   @include('partials.program-section-new')
    @include('partials.image-reviews')
 
 </section>
