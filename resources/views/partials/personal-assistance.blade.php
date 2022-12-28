67c2c60ad568931bae1de4a800923e4ec1b32bc7
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-12-28 21:10:45 +0100

    spacing

--- resources/views/partials/personal-assistance.blade.php
+++ resources/views/partials/personal-assistance.blade.php
@@ -1,5 +1,5 @@
 @if($personalAssistance)
-<div class="font-semibold md:text-xl text-lg mt-2 text-black flex justify-center md:w-11/12 mx-auto">
+<div class="font-semibold md:text-xl text-lg mt-4 text-black flex justify-center md:w-11/12 mx-auto">
 <img class="h-20 mr-1" src="/wp-content/themes/courses-1/resources/images/personal-assistance.png"></img>
     <div class="text-left font-normal !text-sm ml-4">{!! $personalAssistance !!}</div>
 </div>
