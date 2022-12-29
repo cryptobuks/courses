119c6b65337a29485b483ff8499107a1b74c7422
SloveniaDiscovery <tech@slovenia-discovery.com>
2022-12-29 11:00:24 +0100

    css

--- resources/views/partials/image-reviews.blade.php
+++ resources/views/partials/image-reviews.blade.php
@@ -4,8 +4,8 @@
         <img class="md:!h-24 h-14" src="/wp-content/themes/courses-1/resources/images/parenting-award.png"></img>
     </div>
     <div class="flex items-center object-cover	">
-        <img class="md:!h-24 h-10" src="/wp-content/themes/courses-1/resources/images/facebook-reviews.png"></img>
-        <img class="md:!h-20 h-10" src="/wp-content/themes/courses-1/resources/images/google-reviews.png"></img>
-        <img class="md:!h-20 h-10" src="/wp-content/themes/courses-1/resources/images/trustpilot.png"></img>
+        <img class="md:!h-24 h-12" src="/wp-content/themes/courses-1/resources/images/facebook-reviews.png"></img>
+        <img class="md:!h-20 h-12" src="/wp-content/themes/courses-1/resources/images/google-reviews.png"></img>
+        <img class="md:!h-20 h-12" src="/wp-content/themes/courses-1/resources/images/trustpilot.png"></img>
     </div>
 </div>
